3cbcb2a3224b82385a39928688747f6191781e51
Christoffer Illum Søndergaard <sircas@sircas.dk>
2018-10-11 23:12:29 +0200

    Add day 22 part 2 solution helper

--- Day22/DotNet/Program.cs
+++ Day22/DotNet/Program.cs
@@ -2,40 +2,119 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
-namespace Day22
+namespace DotNet
 {
+    public class Node
+    {
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Size { get; set; }
+        public int Used { get; set; }
+        public int Avail { get; set; }
+        public int Use { get; set; }
+    }
+
     public class Program
-    { 
+    {
         public static void Main(string[] args)
         {
             Console.WriteLine("+-------------------------+");
             Console.WriteLine("| Advent of Code - Day 22 |");
             Console.WriteLine("+-------------------------+");
 
-            var input = File
+            var nodes = File
                 .ReadAllLines("input.txt")
                 .Skip(2)
-                .Select(x => x.Split(new [] {' '}, StringSplitOptions.RemoveEmptyEntries))
-                .Select(x => new {
-                    Name  = x[0],
-                    Size  = int.Parse(x[1].Substring(0, x[1].Length - 1)),
-                    Used  = int.Parse(x[2].Substring(0, x[2].Length - 1)),
-                    Avail = int.Parse(x[3].Substring(0, x[3].Length - 1)),
-                    //Use   = int.Parse(x[4].Substring(0, x[4].Length - 1)),
-                    X     = int.Parse(x[0].Substring(16).Split('-')[0]),
-                    Y     = int.Parse(x[0].Split('-')[2].Substring(1))
+                .Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                .Select(x => new Node
+                {
+                    Name = x[0],
+                    X = int.Parse(x[0].Split('-')[1].TrimStart('x')),
+                    Y = int.Parse(x[0].Split('-')[2].TrimStart('y')),
+                    Size = int.Parse(x[1].TrimEnd('T')),
+                    Used = int.Parse(x[2].TrimEnd('T')),
+                    Avail = int.Parse(x[3].TrimEnd('T')),
+                    Use = int.Parse(x[4].TrimEnd('%')),
                 })
                 .ToList();
 
-            var viablePairs = GetPermutations(input, 2)
-                .Select(x => x.ToList())
-                .Count(x =>
-                    x[0].Used > 0 &&
-                    x[0].Used <= x[1].Avail
-                );
+            // Part 1
+            var viablePairs = GetPermutations(nodes, 2)
+              .Select(x => x.ToList())
+              .Count(x =>
+                x[0].Used > 0 &&
+                x[0].Used <= x[1].Avail
+            );
 
             Console.WriteLine($"There is {viablePairs} viable pairs");
+            Console.WriteLine();
+
+            // Part 2
+            var maxX = nodes.Max(x => x.X);
+            var maxY = nodes.Max(y => y.Y);
+
+            var map = new Node[maxX + 1, maxY + 1];
+
+            foreach (var node in nodes)
+            {
+                map[node.X, node.Y] = node;
+            }
+
+            var target = map[maxX, 0];
+
+            for (var y = 0; y < maxY + 1; ++y)
+            {
+                for (var x = 0; x < maxX + 1; ++x)
+                {
+                    var e = map[x, y];
+
+                    if (e.X == 0 && e.Y == 0) // Is goal pos
+                    {
+                        Console.Write($"G ");
+                    }
+                    else if (target.Name == e.Name) // Is target
+                    {
+                        Console.Write($"T ");
+                    }
+                    else if (e.Used == 0) // Is empty space
+                    {
+                        Console.Write($"_ ");
+                    }
+                    else
+                    {
+                        // 1. Check if we're next to an boundary
+                        // 2. If not then determine if we can swap data with every adjacent node
+                        if (
+                          (0 < e.X ? map[e.X - 1, e.Y].Size >= e.Used : true) &&
+                          (e.X < maxX ? map[e.X + 1, e.Y].Size >= e.Used : true) &&
+                          (0 < e.Y ? map[e.X, e.Y - 1].Size >= e.Used : true) &&
+                          (e.Y < maxY ? map[e.X, e.Y + 1].Size >= e.Used : true))
+                        {
+                            // Swap is possible
+                            Console.Write($". ");
+                        }
+                        else
+                        {
+                            // Swap is not possible
+                            Console.Write($"# ");
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"Just count the number of swaps needed to make the target hit the goal");
+            Console.WriteLine($" _ = empty node without data");
+            Console.WriteLine($" . = node which can be swapped in all directions");
+            Console.WriteLine($" # = node which cannot be swapped in all directions");
+            Console.WriteLine($" T = target node with our precious data");
+            Console.WriteLine($" G = goal node which we can read from");
+            Console.WriteLine();
 
             Console.WriteLine($"  -Glædelig jul!");
         }
